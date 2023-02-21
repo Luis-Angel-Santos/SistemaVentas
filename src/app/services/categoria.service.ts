94f3a1cc07263163a0eb388b9f5f0e5cec14bca6
Luis Angel Santos <santosangel748@gmail.com>
2023-02-21 17:05:10 -0600

    Servicio de Categoria

--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ResponseApi } from '../interfaces/response-api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
 
-  constructor() { }
+  private urlAPI: string = environment.endpoint + 'Categoria';
+
+  constructor(private http: HttpClient) { }
+
+  //obtener todas la categorias
+  lista(idUsuario: number):Observable<ResponseApi>{
+    return this.http.get<ResponseApi>(`${this.urlAPI}Lista`);
+  }
 }
