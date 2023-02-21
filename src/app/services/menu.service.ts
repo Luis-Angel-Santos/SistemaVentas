9ba7130aba6895e203bad43a5317e3a4b420eeaa
Luis Angel Santos <santosangel748@gmail.com>
2023-02-21 17:00:05 -0600

    Servicio de Menu

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ResponseApi } from '../interfaces/response-api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
 
-  constructor() { }
+  private urlAPI: string = environment.endpoint + 'Menu';
+
+  constructor(private http: HttpClient) { }
+
+  //obtener los menus asignados a un usuario mediante su id 
+  lista(idUsuario: number):Observable<ResponseApi>{
+    return this.http.get<ResponseApi>(`${this.urlAPI}Lista?idUsuario=${idUsuario}`);
+  }
 }
