72f54427cd5c5be5e4e2d9e32a4d7adff0079c59
Luis Angel Santos <santosangel748@gmail.com>
2023-02-21 16:54:27 -0600

    Servicio de Usuario

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ResponseApi } from '../interfaces/response-api';
+import { Login } from '../interfaces/login';
+import { Usuario } from '../interfaces/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private urlAPI: string = environment.endpoint + 'Usuario';
+
+  constructor(private http: HttpClient) { }
+
+  //iniciar sesion con las credenciales
+  iniciarSesion(request: Login):Observable<ResponseApi>{
+    return this.http.post<ResponseApi>(`${this.urlAPI}IniciarSesion`, request);
+  }
+
+  //obtener todos los usuarios
+  lista():Observable<ResponseApi>{
+    return this.http.get<ResponseApi>(`${this.urlAPI}Lista`);
+  }
+
+  //guardar un nuveo usuario
+  guardar(request: Usuario):Observable<ResponseApi>{
+    return this.http.post<ResponseApi>(`${this.urlAPI}Guardar`, request);
+  }
+
+  //editar un usuario existente
+  editar(request: Usuario):Observable<ResponseApi>{
+    return this.http.put<ResponseApi>(`${this.urlAPI}Editar`, request);
+  }
+
+  //eliminar un usuario mediante su id
+  eliminar(id: number):Observable<ResponseApi>{
+    return this.http.delete<ResponseApi>(`${this.urlAPI}Eliminar/${id}`);
+  }
+}
