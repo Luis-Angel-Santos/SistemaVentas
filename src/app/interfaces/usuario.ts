a1deb294982caead0181c1a8d27f5e70e7727a8f
Luis Angel Santos <santosangel748@gmail.com>
2023-02-21 16:26:45 -0600

    Creando todas las interfaces necesarias

---
+++ src/app/interfaces/usuario.ts
@@ -0,0 +1,9 @@
+export interface Usuario {
+    idUsuario: number,
+    nombreCompleto: string,
+    correo: string,
+    idRol: string,
+    rolDescripcion: string,
+    clave: string,
+    esActivo: number,
+}
