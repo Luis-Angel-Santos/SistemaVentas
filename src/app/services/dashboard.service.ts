59c1a31f4a1ccb4d31d219b04d8203727ead7cee
Luis Angel Santos <santosangel748@gmail.com>
2023-02-21 17:45:54 -0600

    Servicio de Dashboard

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ResponseApi } from '../interfaces/response-api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
 
-  constructor() { }
+  private urlAPI: string = environment.endpoint + 'Dashboard';
+
+  constructor(private http: HttpClient) { }
+
+  //obtener todos los productos
+  resumen():Observable<ResponseApi>{
+    return this.http.get<ResponseApi>(`${this.urlAPI}Resumen`);
+  }
 }
