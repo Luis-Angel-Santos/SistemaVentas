30462122eb3f43d1605d5938a41bc887b13f24a3
Luis Angel Santos <santosangel748@gmail.com>
2023-02-21 17:41:38 -0600

    Servicio de Venta

--- src/app/services/venta.service.ts
+++ src/app/services/venta.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ResponseApi } from '../interfaces/response-api';
+import { Venta } from '../interfaces/venta';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VentaService {
 
-  constructor() { }
+  private urlAPI: string = environment.endpoint + 'Venta';
+
+  constructor(private http: HttpClient) { }
+
+  //registrar una venta
+  registrar(request: Venta):Observable<ResponseApi>{
+    return this.http.post<ResponseApi>(`${this.urlAPI}Registrar`, request);
+  }
+
+  //consultar ventas
+  historial(buscarPor: string, numeroVenta: string, fechaInicio: string, fechaFin: string):Observable<ResponseApi>{
+    return this.http.get<ResponseApi>(`${this.urlAPI}Historial?buscarPor=${buscarPor}&numeroVenta=${numeroVenta}&fechaInicio=${fechaInicio}&fechaFin=${fechaFin}`);
+  }
+
+  //generar reporte de ventas
+  reporte(fechaInicio: string, fechaFin: string):Observable<ResponseApi>{
+    return this.http.get<ResponseApi>(`${this.urlAPI}Reporte?fechaInicio=${fechaInicio}&fechaFin=${fechaFin}`);
+  }
 }
