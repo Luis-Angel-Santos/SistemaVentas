a08482bc959d3f1ba1cd102b714f0457aab4fbcc
Luis Angel Santos <santosangel748@gmail.com>
2023-03-11 17:44:02 -0600

    Fix endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  endpoint: 'http://localhost:5041/api/'
+  endpoint: 'http://sistemaventaslasi.somee.com/api/'
+  //local
+  //endpoint: 'http://localhost:5041/api/'
 };
 
 /*
